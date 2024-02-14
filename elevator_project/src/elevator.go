cefb8225fb2c01dde2f15e8f15f8ba8371410098
innepils <simon@veldig.travel>
2024-02-14 15:30:41 +0100

    Finished translating elevator module

--- elevator_project/src/elevator.go
+++ elevator_project/src/elevator.go
@@ -31,3 +31,57 @@ type Elevator struct {
 	}
 }
 
+func (eb ElevatorBehaviour) String() string {
+	switch eb {
+	case EB_Idle:
+		return "EB_Idle"
+	case EB_DoorOpen:
+		return "EB_DoorOpen"
+	case EB_Moving:
+		return "EB_Moving"
+	default:
+		return "EB_UNDEFINED"
+	}
+}
+
+func (es Elevator) String() string {
+    var b strings.Builder
+    b.WriteString("  +--------------------+\n")
+    b.WriteString(fmt.Sprintf(
+        "  |floor = %-2d          |\n"+
+            "  |dirn  = %-12s|\n"+
+            "  |behav = %-12s|\n",
+        es.Floor, elevio.DirnToString(es.Dirn), es.Behaviour.String(),
+    ))
+    b.WriteString("  +--------------------+\n")
+    b.WriteString("  |  | up  | dn  | cab |\n")
+    for f := N_FLOORS - 1; f >= 0; f-- {
+        b.WriteString(fmt.Sprintf("  | %d", f))
+        for btn := 0; btn < N_BUTTONS; btn++ {
+            if (f == N_FLOORS-1 && btn == B_HallUp) || (f == 0 && btn == B_HallDown) {
+                b.WriteString("|     ")
+            } else {
+                b.WriteString(es.Requests[f][btn] ? "|  #  " : "|  -  ")
+            }
+        }
+        b.WriteString("|\n")
+    }
+    b.WriteString("  +--------------------+\n")
+    return b.String()
+}
+
+func NewUninitializedElevator() Elevator {
+    return Elevator{
+        Floor: -1,
+        Dirn:  D_Stop,
+        Behaviour: EB_Idle,
+        Config: struct {
+            ClearRequestVariant ClearRequestVariant
+            DoorOpenDurationS   float64
+        }{
+            ClearRequestVariant: CV_All,
+            DoorOpenDurationS:   3.0,
+        },
+    }
+}
+
