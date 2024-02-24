b586e594a22abb7f44beb060b795d54b46c416af
innepils <simon@veldig.travel>
2024-02-24 14:51:12 +0100

    syntax changes in main

--- elevator_project/src/config/config.go
+++ elevator_project/src/config/config.go
@@ -1,12 +1,3 @@
 package config
 
 // CONFIGURE SYSTEM
-<<<<<<< HEAD
-=======
-const (
-	NumElevators = 3;
-	NumFloors = 4; //denne ligger i elevator_io_types
-	
-	
-)
->>>>>>> e6c660f77c1982ab7c00a941323c17540521b375
