cf3cc639dc4fad94953463d5795b7cea02750168
innepils <simon@veldig.travel>
2024-02-24 14:07:39 +0100

    Added network packages to main and functionality for assigning ID to the added elevator in main

--- elevator_project/src/main.go
+++ elevator_project/src/main.go
@@ -4,10 +4,33 @@ import (
 	"driver/elevator_io"
 	"driver/elevator_io_types"
 	"driver/fsm"
+	"driver/network/bcast"
+	"driver/network/conn"
+	"driver/network/peers"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
 )
 
 func main() {
-	// Initialize system
+	// Initialize elevator node. The ID can be anything, and should be passed on in the
+	// command line using 'go run main.go -id=our_id'
+
+	var id string
+	flag.StringVar(&id, "ID", "", "ID of this peer")
+	flag.Parse()
+
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+	}
+
 	elevator_io.Init("localhost:15657", elevator_io_types.N_FLOORS)
 
 	// GOROUTINES network
